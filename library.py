038beb1fdb3572add0681528ad30d89d33f0bbe6
Василь Картичак <w.i.k.mailua@gmail.com>
2023-11-06 20:06:29 +0200

    actions cool

--- library.py
+++ library.py
@@ -1,2 +1,3 @@
-def add_two(arg_1fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff: int | str, arg_2: int | str) -> int | str:
-    return arg_1fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff + arg_2
+def add_two(arg_1: int | str, arg_2: int | str) -> int | str:
+    return arg_1 + arg_2
+
